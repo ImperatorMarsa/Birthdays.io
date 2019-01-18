cef3b93d639bf39ba941423b915e5b0a087ec0fe
ImperatorMarsa <tima.sitnikov@mail.ru>
2019-01-18 16:07:25 +0300

    Add files via upload

--- sqlExem.sql
+++ sqlExem.sql
@@ -1,34 +1,3 @@
-
-# Birthdays
-
-This is a small example of a website that allows you to remember the birthdays of employees.
-
-## Installing
-A step by step series of examples that tell you how to get a development this website
-
-### Step 1
-
-If you do not have [Composer](http://getcomposer.org/), you may install it by following the instructions
-at [getcomposer.org](http://getcomposer.org/doc/00-intro.md#installation-nix).
-
-You can then install this project template using the following command:
-
-~~~
-php composer.phar create-project --prefer-dist --stability=dev yiisoft/yii2-app-basic Birthday
-~~~
-
-### Step 2
-Using git write the following command
-~~~
-git clone https://github.com/ImperatorMarsa/Birthdays.io.git
-~~~
-
-### Step 3
-Transfer the contents of folder `Birthdays.io` to folder `Birthday`
-
-### Step 4
-Create a table named `relationship`, `name`, `birthday` in the `yii2basic` database, and insert some sample data. You may run the following SQL statements to do so:
-```sql
 CREATE TABLE `relationship` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`n_id` INT(10) UNSIGNED NULL DEFAULT NULL,
@@ -79,11 +48,11 @@ INSERT INTO `relationship` (`n_id`, `b_id`) VALUES (3, 3);
 INSERT INTO `relationship` (`n_id`, `b_id`) VALUES (4, 4);
 INSERT INTO `relationship` (`n_id`, `b_id`) VALUES (5, 5);
 INSERT INTO `relationship` (`n_id`, `b_id`) VALUES (6, 6);
-```
 
-Now you should be able to access the application through the following URL, assuming `Birthday` is the directory
-directly under the Web root.
 
-~~~
-http://localhost/birthday/web/
-~~~
+SELECT relationship.id, name.name, birthday FROM relationship JOIN name, birthday
+  WHERE name.id=relationship.n_id 
+    AND birthday.id=relationship.b_id
+  
+  ORDER BY name
+;
